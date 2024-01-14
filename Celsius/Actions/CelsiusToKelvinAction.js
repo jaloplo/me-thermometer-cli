8e23fb48d3ab44777cb3e325fbdc00b977a2d647
Jaime López <jaime.lopez.lopez@live.com>
2024-01-15 00:23:23 +0100

    typo: change function name

--- Celsius/Actions/CelsiusToKelvinAction.js
+++ Celsius/Actions/CelsiusToKelvinAction.js
@@ -1,6 +1,6 @@
-function KelvinToCelsiusAction({ degrees }) {
+function CelsiusToKelvinAction({ degrees }) {
     const target = parseFloat(degrees) + 273.15;
     console.log(`${degrees} celsius degrees are ${target} kelvin degrees`);
 }
 
-module.exports = KelvinToCelsiusAction;
+module.exports = CelsiusToKelvinAction;
